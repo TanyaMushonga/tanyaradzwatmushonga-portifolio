564d599a0d8b011e6b8861ba24d72d409fc2a764
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-08-06 14:58:18 +0200

    refactor: Replace Certifications component with Testimonials for improved content flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import About from "./components/about";
 import Awards from "./components/awards/awards";
-import Certifications from "./components/certs/certifications";
+// import Certifications from "./components/certs/certifications";
 import Contact from "./components/contact";
 import Experience from "./components/experience";
 import Hero from "./components/Hero";
 import Projects from "./components/projects";
 import Skills from "./components/skills/Skills";
+import Testimonials from "./components/testimonials";
 
 export default function Home() {
   return (
@@ -16,7 +17,8 @@ export default function Home() {
       <Projects />
       <Experience />
       <Awards />
-      <Certifications />
+      <Testimonials />
+      {/* <Certifications /> */}
       <Contact />
     </main>
   );
