5a49565ea1451e0155a1fa05c1a9f50e754331ea
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-12-26 12:23:59 +0200

    feat: Add Hackathons and Research sections, and enhance project filtering with priority sorting.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,28 @@
-import About from "./components/about";
-import Awards from "./components/awards/awards";
-// import Certifications from "./components/certs/certifications";
+import Research from "./components/research/Research";
+import Hackathons from "./components/hackathons/Hackathons";
+import Certifications from "./components/certs/certifications";
 import Contact from "./components/contact";
 import Experience from "./components/experience";
 import Hero from "./components/Hero";
 import Projects from "./components/projects";
 import Skills from "./components/skills/Skills";
 import Testimonials from "./components/testimonials";
+import About from "./components/about";
+import Awards from "./components/awards/awards";
 
 export default function Home() {
   return (
     <main className="min-h-screen ">
       <Hero />
       <About />
-      <Skills />
       <Projects />
+      <Skills />
       <Experience />
+      <Research />
+      {/* <Hackathons /> */}
+      <Certifications />
       <Awards />
       <Testimonials />
-      {/* <Certifications /> */}
       <Contact />
     </main>
   );
